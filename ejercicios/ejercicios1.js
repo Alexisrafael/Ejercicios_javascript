477b55858ad8b8d0f6a2199e59fdaa297005b4fc
alexis <alexis.pina@handelbay.com>
2024-04-05 02:12:41 -0500

    se completan los 3 primeros ejercicios

--- ejercicios/ejercicios1.js
+++ ejercicios/ejercicios1.js
@@ -4,15 +4,23 @@ function verificar_si_es_vocal(params) {
     // 1. Se debe retornar true cuando el parametro sea una vocal
     // 2. Si el parametro es una consonante debe retornar false
     // 3. Si el parametro incresado es un numero debe retornar 'Lo sentimos pero solo puedes incresar letras'
+
+}
+
+function entrar_en_la_discoteca(params) {
+    // Se necesita verificar la edad de las personas que entran en la discoteca
+    // por ende las personas con una edad menor a 18 años se debe mostrar este mensaje "No puedes ingresar a la discoteca", si la 
+    // persona tiene entre 18 y 55 años "Puedes ingresar a las discoteca", si es mayor se le 
+    // debe indicar que es "fiesta privada".
     
 }
 
 function multiplica_dos_numeros_sin_usar_el_operador_matematico(num1, num2) {
-    // Se debe realizar una multiplicacion sin usar el operador matematico *
+    // Se debe realizar una multiplicacion sin usar el operador matematico
     // Y que el campo este limitado a numeros con 3 digitos
     // si ingresan numeros mayores a 3 digitos mostrar en pantalla 'No puede ingresar numeros con mas de 3 digitos'
     
 }
 
 
-module.exports = {verificar_si_es_vocal, multiplica_dos_numeros_sin_usar_el_operador_matematico }
+module.exports = {verificar_si_es_vocal, entrar_en_la_discoteca, multiplica_dos_numeros_sin_usar_el_operador_matematico }
--- test/ejercicios1.spec.js
+++ test/ejercicios1.spec.js
@@ -1,4 +1,4 @@
-const {verificar_si_es_vocal, multiplica_dos_numeros_sin_usar_el_operador_matematico} = require('../ejercicios/ejercicios1');
+const {verificar_si_es_vocal, entrar_en_la_discoteca, multiplica_dos_numeros_sin_usar_el_operador_matematico} = require('../ejercicios/ejercicios1');
 
 describe('Verificar si es vocal', ()=>{
     it('Debe retornar true cuando sean vocales', ()=>{
@@ -12,20 +12,31 @@ describe('Verificar si es vocal', ()=>{
     });
 })
 
-const originalMultiplicacion = multiplica_dos_numeros_sin_usar_el_operador_matematico.multiplicacion;
-multiplica_dos_numeros_sin_usar_el_operador_matematico.multiplicacion = jest.fn((a, b) => {
-  throw new Error('No se permite usar el operador *');
-});
+describe('Verificar la edades de las personas que entra en la discoteca', ()=>{
+    it('No pueden ingreasar si son menores de edad', () => {
+        expect(entrar_en_la_discoteca(15)).toBe('No puedes ingresar a la discoteca')
+    });
+
+    it('Si tienen entre 18 y 55 años puede ingresar', ()=>{
+        expect(entrar_en_la_discoteca(18)).toBe('Puedes ingresar a las discoteca')
+    });
+
+    it('Si es mayor a 55 años ', ()=>{
+        expect(entrar_en_la_discoteca(60)).toBe('fiesta privada')
+    });
+})
 
 describe('Multiplica dos numeros sin usar el operador matematico', ()=>{
+    it('No debe usar el operador matematico', () => {
+        const funcString = multiplica_dos_numeros_sin_usar_el_operador_matematico.toString();
+        const usesMathOperator = funcString.includes('*');
+        expect(usesMathOperator).toBe(false);
+    });
+
     it('No puede ingresar numeros con mas de 3 digitos', ()=>{
         expect(multiplica_dos_numeros_sin_usar_el_operador_matematico(1000, 1000)).toBe('No puede ingresar numeros con mas de 3 digitos')
     });
-    it('No se permite usar el operador *', ()=>{
-        expect(() => {
-            multiplica_dos_numeros_sin_usar_el_operador_matematico.multiplicacion(2, 3);
-          }).toThrow('No se permite usar el operador *');
-    });
+
     it('Debería permitir la multiplicación original', ()=>{
         expect(multiplica_dos_numeros_sin_usar_el_operador_matematico(2,3)).toBe(6)
     });
